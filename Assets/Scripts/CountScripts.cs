f9a76a90c5a5785597813e8b12947ac6b6d3f233
NANI-PC\NANI <takegiyoshimoto@gmail.com>
2018-08-10 03:11:30 +0900

    初めてのコミット

---
+++ Assets/Scripts/CountScripts.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountScripts : MonoBehaviour {
+
+
+	public  int number;
+	public Text text;
+	public  Text  damage;
+	public int total;
+	public AudioSource sound;
+
+	// Use this for initialization
+	void Start () {
+		number = 0;
+		sound = GetComponent<AudioSource> ();
+	
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (number >= 100) {
+			Application.LoadLevel ("GameClear");
+		}
+		
+	}
+	public void CountUp(){
+		number = number + 1;
+		text.text = number.ToString ();
+	}
+	public void Attack(){
+		total += number;
+		damage.text = number.ToString ();
+		sound.PlayOneShot (sound.clip);
+
+
+	}
+			
+
+}
